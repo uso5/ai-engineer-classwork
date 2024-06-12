19d4754a7e99ee55566ba1d4598b06723b040651
Michal Ustanik <michal.ustanik@gmail.com>
2024-06-12 17:22:36 +0200

    lesson 2 - snake

---
+++ 2_customize_model/3_skills/rest/script.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "https://petstore.swagger.io/v2/store/order"
+
+payload = {
+    "id": 0,
+    "petId": 0,
+    "quantity": 1,
+    "shipDate": "2024-06-12T12:34:56.789Z",
+    "status": "placed",
+    "complete": True
+}
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.text)
